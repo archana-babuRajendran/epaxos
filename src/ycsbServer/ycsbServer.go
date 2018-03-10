332c37333781d7ac1109017c0edb3d7ef61403ab
Archana Babu Rajendran <arch.seven94@gmail.com>
2018-03-09 22:10:04 -0800

    Executing client command from ycsbserver

--- src/ycsbServer/ycsbServer.go
+++ src/ycsbServer/ycsbServer.go
@@ -2,11 +2,15 @@ package main
 
 import (
   "log"
-  "net/http"
+  "net/http",
+  "client",
+  "os/exec"
 )
 
 func dummyHandler(w http.ResponseWriter, r *http.Request) {
   log.Println("Fielding request")
+  cmnd := exec.Command("../client/client", '-maddr "10.0.0.10" -mport 7087 -q 100 -c 100 -e true')
+  cmnd.Start()
   w.Write([]byte("Youve reached state"))
 }
 
